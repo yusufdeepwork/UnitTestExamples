553ca4e58e1b05ce756ba18cf78bd65e8d46337a
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-02-21 13:16:57 +0300

    Prime Factor Class & Unit Test

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class PrimeFactors {
-}
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+
+
+
+
+    public List<Integer> generate(int number){
+        List<Integer> primeFactors = new ArrayList<>();
+        int checkPrimeNumber = 2;
+        for (int i = 0; i <= number ; i++) {
+            //the 1 isn't prime number.
+            if (number == 1)
+                break;
+            // it increases for finding prime number
+            while (number % checkPrimeNumber != 0)
+                checkPrimeNumber++;
+            // If It finds prime number, It divides number for accessing true result.
+            while(number % checkPrimeNumber == 0)
+               number = number / checkPrimeNumber;
+
+            //It adds prime factor to list
+            primeFactors.add(checkPrimeNumber);
+
+            }
+        return primeFactors;
+        }
+    }
--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,7 +1,62 @@
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
+@DisplayName(" When We sent a number, It calculates prime factor and return integer list.")
 class PrimeFactorsTest {
 
-    @org.junit.jupiter.api.Test
-    void generate() {
+    private List<Integer> realPrimeNumbers = new ArrayList<>();
+    private PrimeFactors primeFactors = new PrimeFactors();
+
+    @BeforeEach
+    void clearPrimeNumbersList() {
+        realPrimeNumbers.clear();
+    }
+
+
+    @Test
+    @DisplayName(" When We sent a number that has  just a reminder number, It calculates prime factor as a itself ")
+    void whenItselfPrimeNumberThenCalculatePrimeFactors() {
+
+        realPrimeNumbers.add(11);
+        assertEquals(realPrimeNumbers,primeFactors.generate(11));
+
+        realPrimeNumbers.clear();
+        realPrimeNumbers.add(7);
+        assertEquals(realPrimeNumbers,primeFactors.generate(7));
+
+        realPrimeNumbers.clear();
+        realPrimeNumbers.add(17);
+        assertEquals(realPrimeNumbers,primeFactors.generate(17));
+
+    }
+    @Test
+    @DisplayName(" When We sent a number that has  both more than one reminder number, It calculates prime factor ")
+
+    //For example 36 = 2*2*3*3 It's prime numbers are 2,3
+    void whenNormalNumberThenCalculatePrimeFactors() {
+
+        realPrimeNumbers.add(2);
+        realPrimeNumbers.add(3);
+        assertEquals(realPrimeNumbers,primeFactors.generate(36));
+        realPrimeNumbers.clear();
+
+        realPrimeNumbers.add(5);
+        realPrimeNumbers.add(7);
+        //5*5*7*7 = 1225
+        assertEquals(realPrimeNumbers,primeFactors.generate(1225));
+    }
+
+    @Test
+    @DisplayName(" When We sent 1 It doesnt calculate prime factor ")
+    void whenTheOneThenDoesntCalculatePrimeFactors() {
+
+        assertEquals(realPrimeNumbers,primeFactors.generate(1));
+
     }
 }
